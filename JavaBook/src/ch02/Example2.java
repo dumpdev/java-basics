b747a4540e2356fcd82c5979c1fd03a84a7d255c
dumpdev <amkhan_08@yahoo.com>
2023-01-02 12:51:55 +0530

    update

---
+++ JavaBook/src/ch02/Example2.java
@@ -0,0 +1,14 @@
+package ch02;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		num = 50;
+		System.out.println("This is Num: " + num);
+		num = num * 2;
+		System.out.println("The Value of Num X 2 is:" + num);
+	}
+
+}
